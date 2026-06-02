435ee4edafbdddea6458841ded81da787aa3336f
Jeff Cameron <jeff@maui.local>
2026-06-01 20:23:02 -0400

    Set up browserify.

---
+++ cave3d.js
@@ -0,0 +1,19 @@
+const { createNoise3D } = require('simplex-noise');
+
+const noise3D = createNoise3D();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  draw();
+}
+
+function draw() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+window.addEventListener('resize', resize);
+resize();
